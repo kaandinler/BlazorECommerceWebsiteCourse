e065290b079f36e986fdd88ea1e1e157ad0871bb
Kaan Dinler <kaan_d_tr@hotmail.com>
2022-08-31 00:38:42 +0300

    Add project files.

---
+++ BlazorECommerceWebsiteCourse/Server/Data/DataContext.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BlazorECommerceWebsiteCourse.Server.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().HasData(
+                 new Product
+                 {
+                     id = 1,
+                     Title = "Breaking Bad",
+                     Description = "A high school chemistry teacher diagnosed with inoperable lung cancer turns to manufacturing and selling methamphetamine in order to secure his family's future.",
+                     ImageUrl = "https://flxt.tmsimg.com/assets/p185846_b_h9_ad.jpg",
+                     Price = 9.99m
+                 },
+                    new Product
+                    {
+                        id = 2,
+                        Title = "Altered Carbon",
+                        Description = "Set in a future where consciousness is digitized and stored, a prisoner returns to life in a new body and must solve a mind-bending murder to win his freedom.",
+                        ImageUrl = "https://images.squarespace-cdn.com/content/v1/540efafce4b02e0dc20adb52/1520180035309-DVO0IGJFKF69ITB31Z6U/alteredcarbon.jpg?format=1500w",
+                        Price = 4.99m
+                    },
+                    new Product
+                    {
+                        id = 3,
+                        Title = "The Office",
+                        Description = "A mockumentary on a group of typical office workers, where the workday consists of ego clashes, inappropriate behavior, and tedium.",
+                        ImageUrl = "https://m.media-amazon.com/images/M/MV5BMDNkOTE4NDQtMTNmYi00MWE0LWE4ZTktYTc0NzhhNWIzNzJiXkEyXkFqcGdeQXVyMzQ2MDI5NjU@._V1_FMjpg_UX1000_.jpg",
+                        Price = 5.99m
+                    }
+                );
+        }
+
+        //DB Table of Product
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ BlazorECommerceWebsiteCourse/Server/Migrations/20220830182205_ProductSeeding.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlazorECommerceWebsiteCourse.Server.Migrations
+{
+    public partial class ProductSeeding : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "id", "Description", "ImageUrl", "Price", "Title" },
+                values: new object[] { 1, "A high school chemistry teacher diagnosed with inoperable lung cancer turns to manufacturing and selling methamphetamine in order to secure his family's future.", "https://flxt.tmsimg.com/assets/p185846_b_h9_ad.jpg", 9.99m, "Breaking Bad" });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "id", "Description", "ImageUrl", "Price", "Title" },
+                values: new object[] { 2, "Set in a future where consciousness is digitized and stored, a prisoner returns to life in a new body and must solve a mind-bending murder to win his freedom.", "https://images.squarespace-cdn.com/content/v1/540efafce4b02e0dc20adb52/1520180035309-DVO0IGJFKF69ITB31Z6U/alteredcarbon.jpg?format=1500w", 4.99m, "Altered Carbon" });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "id", "Description", "ImageUrl", "Price", "Title" },
+                values: new object[] { 3, "A mockumentary on a group of typical office workers, where the workday consists of ego clashes, inappropriate behavior, and tedium.", "https://m.media-amazon.com/images/M/MV5BMDNkOTE4NDQtMTNmYi00MWE0LWE4ZTktYTc0NzhhNWIzNzJiXkEyXkFqcGdeQXVyMzQ2MDI5NjU@._V1_FMjpg_UX1000_.jpg", 5.99m, "The Office" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "id",
+                keyValue: 3);
+        }
+    }
+}
